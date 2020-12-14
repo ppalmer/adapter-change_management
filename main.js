1ae036f85841627f845a77eac5934399bae111e6
ppalmer <ppalmer04@gmail.com>
2020-12-14 16:13:32 +0000

    Final Project

--- main.js
+++ main.js
@@ -211,7 +211,7 @@ healthcheck(callback) {
         const change_body = JSON.parse(data.body);
         //change_body = data.body;
         const change_result = change_body.result;
-        console.log(`CHANGE TICKET RESULT: ${JSON.parse(change_result)}`);
+        console.log(`CHANGE TICKET RESULT: ${change_result}`);
         returned_object = {change_ticket_number: change_result[0].number,
                            active: change_result[0].active,
                            priority: change_result[0].priority,
